b1dba4de3c233c3b13fef5dde68b72df0396c10d
Guga2111 <luisgosampaio@gmail.com>
2026-07-19 19:19:35 -0300

    feat(api): initialize Spring Boot project with Maven

---
+++ api/src/main/java/com/lf/sessao_a_dois/SessaoADoisApplication.java
@@ -0,0 +1,13 @@
+package com.lf.sessao_a_dois;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaoADoisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaoADoisApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/lf/sessao_a_dois/SessaoADoisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lf.sessao_a_dois;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessaoADoisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
