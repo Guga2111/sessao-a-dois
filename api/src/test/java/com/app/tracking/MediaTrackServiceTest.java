29a7f75e00c8e739c6c1b1d5b3d67db8a6919045
Guga2111 <luisgosampaio@gmail.com>
2026-07-22 16:08:08 -0300

    fix: missing import

--- api/src/test/java/com/app/tracking/MediaTrackServiceTest.java
+++ api/src/test/java/com/app/tracking/MediaTrackServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
